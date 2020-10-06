6ce74b83b0b4cd785880771bb818e890b748f9b1
afitra <loeby45@gmail.com>
2020-10-06 21:03:19 +0700

    create template

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import logo from "./logo.svg"
+import "./assets/scss/style.scss"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Route> */}
+    </div>
+  )
+}
+
+export default App
